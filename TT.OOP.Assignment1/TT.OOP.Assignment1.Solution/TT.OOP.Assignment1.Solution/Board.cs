1fbad9faed8b603578831ab8eeffe57a27b69aa4
Gaoxin Huang <gx173139880@qq.com>
2022-02-23 22:16:46 +1300

    PracticeDay1

---
+++ TT.OOP.Assignment1/TT.OOP.Assignment1.Solution/TT.OOP.Assignment1.Solution/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TT.OOP.Assignment1.Solution
+{
+    public class Board
+    {
+        int Row;
+        int Column;
+        public Board()
+        {
+
+        }
+    }
+}
---
+++ TT.OOP.Assignment1/TT.OOP.Assignment1.Solution/TT.OOP.Assignment1.Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TT.OOP.Assignment1.Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
